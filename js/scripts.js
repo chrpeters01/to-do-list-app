2dd61f3643b7c34e1195e9e2cb46651739c8b8ed
chrpeters01 <chrisbpeters01@gmail.com>
2023-10-09 13:38:59 -0500

    update css
    format css

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function newItem() {
+
+    // adding new item to list: 
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
+  
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
+  
+    // to strike item on list:
+    function crossOut() {
+      li.toggleClass("strike");
+    }
+  
+    li.on("dblclick", function crossOut() {
+      li.toggleClass("strike")
+    });
+  
+    // adding delete button "X": 
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+  
+    crossOutButton.on("click", deleteListItem);
+  
+    function deleteListItem() {
+      li.addClass("delete")
+    }
+    // re-order items: 
+    $('#list').sortable();
+  
+  }
